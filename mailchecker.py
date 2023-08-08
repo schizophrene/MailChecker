1b04892f885f9ad457b69dcb7c04e8c749e776ed
schizophrene <140683437+schizophrene@users.noreply.github.com>
2023-08-08 15:31:11 +0200

    Add files via upload

---
+++ mailchecker.py
@@ -0,0 +1,50 @@
+import requests
+import time as time
+from bs4 import BeautifulSoup
+
+print("""
+███╗   ███╗ █████╗ ██╗██╗          ██████╗██╗  ██╗███████╗ ██████╗██╗  ██╗███████╗██████╗ 
+████╗ ████║██╔══██╗██║██║         ██╔════╝██║  ██║██╔════╝██╔════╝██║ ██╔╝██╔════╝██╔══██╗
+██╔████╔██║███████║██║██║         ██║     ███████║█████╗  ██║     █████╔╝ █████╗  ██████╔╝
+██║╚██╔╝██║██╔══██║██║██║         ██║     ██╔══██║██╔══╝  ██║     ██╔═██╗ ██╔══╝  ██╔══██╗
+██║ ╚═╝ ██║██║  ██║██║███████╗    ╚██████╗██║  ██║███████╗╚██████╗██║  ██╗███████╗██║  ██║
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+                                                                          by schizophrenic
+     
+""")
+
+def main():
+    option = input("""
+[1] Check si un mail existe
+[2] Générer des combinaisons d'un mail et check si ils existent
+[3] Menu d'aide
+""")
+    if option == "1":
+        mail = input("Veuillez entrer le nom d'un gmail: ")
+        page = requests.get(f"https://mailscrap.com/api/verifier-lookup/{mail}@gmail.com")
+        soup = BeautifulSoup(page.content, "html.parser")
+        if soup is not None:
+            if '"deliverable":true' in str(soup):
+                print(f"[+] {mail}@gmail.com")
+            else:
+                print(f"[-] {mail}@gmail.com")
+    elif option == "2":
+        mail = input("Veuillez entrer le nom d'un gmail: ")
+        i = 000
+        while i < 999:
+            page = requests.get(f"https://mailscrap.com/api/verifier-lookup/{mail}{i}@gmail.com")
+            soup = BeautifulSoup(page.content, "html.parser")
+            if soup is not None:
+                if '"deliverable":true' in str(soup):
+                    print(f"[+] {mail}{i}@gmail.com")
+                else:
+                    print(f"[-] {mail}{i}@gmail.com")
+            i += 1
+    elif option == "3":
+        print("Pour utiliser le tool il faut entrer le nom d'un gmail par exemple pour schizophrenic@gmail.com, entrez schizophrenic")
+        return main()
+    else:
+        print("Option Invalide")
+        return main()
+
+main()
